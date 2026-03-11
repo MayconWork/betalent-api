a1ce97cedb3fab959c901bbe9dd88b0b641c17da
MayconWork <maycon.oliveira.work@gmail.com>
2026-03-11 16:27:52 -0300

    feat(product): add product model and test

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -3,18 +3,32 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Product;
 
 class ProductTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_product_can_be_created()
+    {
+        $product = Product::create([
+            'name' => 'Notebook',
+            'amount' => 100000  // Valor em centavos
+        ]);
+
+        $this->assertDataBaseHas('products', [
+            'name' => 'Notebook'
+        ]);
     }
+
+    public function test_product_amount_is_interger()
+        {
+            $product = Product::create([
+                'name' => 'Mouse',
+                'amount' => 5000
+            ]);
+
+            $this->assertIsInt($product->amount);
+        }
 }
