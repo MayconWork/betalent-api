990f3c4f756e3f8bd043e9e479618023bcd515b8
MayconWork <maycon.oliveira.work@gmail.com>
2026-03-12 10:02:19 -0300

    test(Purchase): add Purchase test

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PurchaseController;
 
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/purchase', [PurchaseController::class, 'purchase']);
--- tests/Feature/PurchaseTest.php
+++ tests/Feature/PurchaseTest.php
@@ -2,18 +2,26 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Product;
 
 class PurchaseTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    public function test_user_can_purchase()
     {
-        $response = $this->get('/');
+        $product = Product::create([
+            'name' => 'Test Product',
+            'amount' => 1000
+        ]);
+
+        $response = $this->postJson('/api/purchase', [
+            'product_id' => $product->id,
+            'quantity' => 1,
+            'name' => 'Tester',
+            'email' => 'tester@email.com',
+            'cardNumber' => '5569000000006063',
+            'cvv' => '010'
+        ]);
 
         $response->assertStatus(200);
     }
